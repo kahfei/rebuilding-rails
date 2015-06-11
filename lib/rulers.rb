3aa2fc06064fb87d8bdbc2f17949b017f63eb177
kahfei <bak.kahfei@gmail.com>
2015-06-11 17:12:19 +0800

    update

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,8 +4,21 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200,{'Content-Type'=>'text/html'},["Hello World"]]
     end
   end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
+    end
+  end
+
 end
